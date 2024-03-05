64ca2279c90e88c8988c929b09eaba2e0d3302f3
theshubz <157612314+theshubz@users.noreply.github.com>
2024-03-05 12:44:18 +0530

    updated

--- company.php
+++ company.php
@@ -1,29 +1,33 @@
-
-    <section id="content">
-        <div class="container content">     
-        
+<section id="content">
+    <div class="container content">     
         <div class="row">
             <?php 
-                  $sql = "SELECT * FROM `tblcompany`";
-                  $mydb->setQuery($sql);
-                  $comp = $mydb->loadResultList(); 
+            require_once('include/database.php');
 
-                  foreach ($comp as $company ) { 
+            // Assuming $mydb is a MySQLi database connection object
+            $sql = "SELECT * FROM `tblcompany`";
+            $result = $mydb->query($sql);
+
+            if ($result) {
+                while ($company = $result->fetch_object()) {
             ?>
                     <div class="col-sm-4 info-blocks">
                         <i class="icon-info-blocks fa fa-building-o"></i>
                         <div class="info-blocks-in">
-                            <h3><?php echo '<a href="'.web_root.'index.php?q=hiring&search='.$company->COMPANYNAME.'">'.$company->COMPANYNAME.'</a>';?></h3>
+                            <h3>
+                                <?php echo '<a href="'.web_root.'index.php?q=hiring&search='.$company->COMPANYNAME.'">'.$company->COMPANYNAME.'</a>';?>
+                            </h3>
                             <!-- <p><?php echo $company->COMPANYMISSION;?></p> -->
-                            <p>Address :<?php echo $company->COMPANYADDRESS;?></p>
-                            <p>Contact No. :<?php echo $company->COMPANYCONTACTNO;?></p>
+                            <p>Address: <?php echo $company->COMPANYADDRESS;?></p>
+                            <p>Contact No.: <?php echo $company->COMPANYCONTACTNO;?></p>
                         </div>
                     </div>
-
-            <?php } ?>
-
- 
- 
-         </div> 
-        </div>
-    </section>
+            <?php 
+                } // End of foreach loop
+            } else {
+                echo "Error: " . $mydb->error;
+            }
+            ?>
+        </div> 
+    </div>
+</section>
--- include/companies.php
+++ include/companies.php
@@ -1,5 +1,5 @@
 <?php
-require_once('database.php');
+require_once('include/database.php');
 class Company {
 	protected static  $tblname = "tblcompany";
 
