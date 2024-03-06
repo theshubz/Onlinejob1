be0b96ccaae1f5ed077a193ae3fe0cd84ed34d04
theshubz <157612314+theshubz@users.noreply.github.com>
2024-03-06 23:00:38 +0530

    updated

--- process.php
+++ process.php
@@ -530,47 +530,36 @@ function doUpdate($jobid=0,$fileid=0) {
 	}
 }
 function doRegister(){
-	
 	if (isset($_POST['btnRegister'])) { 
-			$birthdate =  $_POST['year'].'-'.$_POST['month'].'-'.$_POST['day'];
-
-			$age = date_diff(date_create($birthdate),date_create('today'))->y;
-
-			if ($age < 20){
+		$birthdate = $_POST['year'].'-'.$_POST['month'].'-'.$_POST['day'];
+	
+		$age = date_diff(date_create($birthdate), date_create('today'))->y;
+	
+		if ($age < 20) {
 			message("Invalid age. 20 years old and above is allowed.", "error");
 			redirect("index.php?q=register");
-
-			}else{
-
-			$autonum = New Autonumber();
+		} else {
+			$autonum = new Autonumber();
 			$auto = $autonum->set_autonumber('APPLICANT');
 			 
-			$applicant =New Applicants();
-			$applicant->APPLICANTID = date('Y').$auto->AUTO;
-			$applicant->FNAME = $_POST['FNAME'];
-			$applicant->LNAME = $_POST['LNAME'];
-			$applicant->MNAME = $_POST['MNAME'];
-			$applicant->ADDRESS = $_POST['ADDRESS'];
-			$applicant->SEX = $_POST['optionsRadios'];
-			$applicant->CIVILSTATUS = $_POST['CIVILSTATUS'];
-			$applicant->BIRTHDATE = $birthdate;
-			$applicant->BIRTHPLACE = $_POST['BIRTHPLACE'];
-			$applicant->AGE = $age;
-			$applicant->USERNAME = $_POST['USERNAME'];
-			$applicant->PASS = sha1($_POST['PASS']);
-			$applicant->EMAILADDRESS = $_POST['EMAILADDRESS'];
-			$applicant->CONTACTNO = $_POST['TELNO'];
-			$applicant->DEGREE = $_POST['DEGREE'];
-			$applicant->create();
-
-			$autonum = New Autonumber();
-		
-
-			$sql = "INSERT INTO tblapplicants (APPLICANTID, FNAME, LNAME, MNAME, ADDRESS, SEX, CIVILSTATUS, BIRTHDATE, BIRTHPLACE, AGE, USERNAME, PASS, EMAILADDRESS, CONTACTNO, DEGREE) VALUES ('$applicantId', '$fname', '$lname', '$mname', '$address', '$sex', '$civilStatus', '$birthdate', '$birthplace', '$age', '$username', '$password', '$email', '$contactNo', '$degree')";
-
-			$mydb->setQuery($sql);
+			$applicantId = date('Y').$auto->AUTO;
+			$fname = $_POST['FNAME'];
+			$lname = $_POST['LNAME'];
+			$mname = $_POST['MNAME'];
+			$address = $_POST['ADDRESS'];
+			$sex = $_POST['optionsRadios'];
+			$civilStatus = $_POST['CIVILSTATUS'];
+			$birthplace = $_POST['BIRTHPLACE'];
+			$username = $_POST['USERNAME'];
+			$password = sha1($_POST['PASS']);
+			$email = $_POST['EMAILADDRESS'];
+			$contactNo = $_POST['TELNO'];
+			$degree = $_POST['DEGREE'];
 	
-			if ($mydb->executeQuery()) {
+			$sql = "INSERT INTO tblapplicants (APPLICANTID, FNAME, LNAME, MNAME, ADDRESS, SEX, CIVILSTATUS, BIRTHDATE, BIRTHPLACE, AGE, USERNAME, PASS, EMAILADDRESS, CONTACTNO, DEGREE) VALUES ('$applicantId', '$fname', '$lname', '$mname', '$address', '$sex', '$civilStatus', '$birthdate', '$birthplace', '$age', '$username', '$password', '$email', '$contactNo', '$degree')";
+	
+			// Execute the query directly using the Database class
+			if ($mydb->query($sql)) {
 				message("You are successfully registered to the site. You can login now!", "success");
 				redirect("index.php?q=success");
 			} else {
@@ -578,9 +567,9 @@ function doRegister(){
 				message("Error: Failed to register. Please try again later.", "error");
 				redirect("index.php?q=register");
 			}
-			
-	 }
-}
+		}
+	}
+	
 }
 
 function doLogin(){
