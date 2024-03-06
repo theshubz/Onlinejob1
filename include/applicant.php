280be2cc9b6bdc64a39dc2d8e7108b52841acad3
theshubz <157612314+theshubz@users.noreply.github.com>
2024-03-06 09:32:23 +0530

    update1 applicant

--- include/applicant.php
+++ include/applicant.php
@@ -1,193 +1,174 @@
 <?php
 require_once('database.php');
+
 class Applicants {
-	protected static  $tblname = "tblapplicants";
+    protected static $tblname = "tblapplicants";
 
-	function dbfields () {
-		global $mydb;
-		return $mydb->getfieldsononetable(self::$tblname);
+    public function dbfields() {
+        global $mydb;
+        return $mydb->getfieldsononetable(self::$tblname);
+    }
 
-	}
-	function listofapplicant(){
-		global $mydb;
-		$mydb->setQuery("SELECT * FROM ".self::$tblname);
-		return $cur;
-	}
-	function find_applicant($id="",$name=""){
-		global $mydb;
-		$mydb->setQuery("SELECT * FROM ".self::$tblname." 
-			WHERE APPLICANTID = {$id} OR Lastname = '{$name}'");
-		$cur = $mydb->executeQuery();
-		$row_count = $mydb->num_rows($cur);
-		return $row_count;
-	}
+    public function listofapplicant() {
+        global $mydb;
+        $result = $mydb->query("SELECT * FROM " . self::$tblname);
+        return $result;
+    }
 
-	function find_all_applicant($lname="",$Firstname="",$mname=""){
-		global $mydb;
-		$mydb->setQuery("SELECT * FROM ".self::$tblname." 
-			WHERE LNAME = '{$lname}' AND FNAME= '{$Firstname}' AND MNAME='{$mname}'");
-		$cur = $mydb->executeQuery();
-		$row_count = $mydb->num_rows($cur);
-		return $row_count;
-	}
-	 
-	function single_applicant($id=""){
-			global $mydb;
-			$mydb->setQuery("SELECT * FROM ".self::$tblname." 
-				Where APPLICANTID= '{$id}' LIMIT 1");
-			$cur = $mydb->loadSingleResult();
-			return $cur;
-	}
-	function select_applicant($id=""){
-			global $mydb;
-			$mydb->setQuery("SELECT * FROM ".self::$tblname." 
-				Where APPLICANTID= '{$id}' LIMIT 1");
-			$cur = $mydb->loadSingleResult();
-			return $cur;
-	}
-	function applicantAuthentication($U_USERNAME, $h_pass) {
-		$sql = "SELECT * FROM `tblapplicants` WHERE `USERNAME`=? AND `PASS`=?";
-		$params = array($U_USERNAME, $h_pass);
-	
-		$stmt = $conn->prepare($sql);
-	
-		if ($stmt === false) {
-			die("Error preparing statement: " . $conn->error);
-		}
-	
-		$stmt->bind_param("ss", ...$params);
-		$stmt->execute();
-		$result = $stmt->get_result();
-	
-		if ($result !== false) {
-			$row_count = $result->num_rows;
-			if ($row_count == 1) {
-				$emp_found = $result->fetch_object(); 
-				$_SESSION['APPLICANTID'] = $emp_found->APPLICANTID; 
-				$_SESSION['USERNAME'] = $emp_found->USERNAME;  
-				$result->close(); // Close the result set
-				return true;
-			} else {
-				$result->close(); // Close the result set
-				return false;
-			}
-		} else {
-			die("Error executing query: " . $conn->error);
-		}
-	}
-	
-	
-	
+    public function find_applicant($id = "", $name = "") {
+        global $mydb;
+        $result = $mydb->query("SELECT * FROM " . self::$tblname . " WHERE APPLICANTID = '{$id}' OR Lastname = '{$name}'");
+        $row_count = $result->num_rows;
+        return $row_count;
+    }
 
-	 
+    public function find_all_applicant($lname = "", $Firstname = "", $mname = "") {
+        global $mydb;
+        $result = $mydb->query("SELECT * FROM " . self::$tblname . " WHERE LNAME = '{$lname}' AND FNAME= '{$Firstname}' AND MNAME='{$mname}'");
+        $row_count = $result->num_rows;
+        return $row_count;
+    }
 
-	static function instantiate($record) {
-		$object = new self;
+    public function single_applicant($id = "") {
+        global $mydb;
+        $result = $mydb->query("SELECT * FROM " . self::$tblname . " WHERE APPLICANTID= '{$id}' LIMIT 1");
+        $cur = $result->fetch_assoc();
+        return $cur;
+    }
 
-		foreach($record as $attribute=>$value){
-		  if($object->has_attribute($attribute)) {
-		    $object->$attribute = $value;
-		  }
-		} 
-		return $object;
-	}
-	
-	
-	
-	private function has_attribute($attribute) {
+    public function select_applicant($id = "") {
+        global $mydb;
+        $result = $mydb->query("SELECT * FROM " . self::$tblname . " WHERE APPLICANTID= '{$id}' LIMIT 1");
+        $cur = $result->fetch_assoc();
+        return $cur;
+    }
 
-	  return array_key_exists($attribute, $this->attributes());
-	}
+    public function applicantAuthentication($U_USERNAME, $h_pass) {
+        global $mydb;
+        $sql = "SELECT * FROM `tblapplicants` WHERE `USERNAME`=? AND `PASS`=?";
+        $stmt = $mydb->prepare($sql);
 
-	protected function attributes() { 
-		// return an array of attribute names and their values
-	  global $mydb;
-	  $attributes = array();
-	  foreach($this->dbfields() as $field) {
-	    if(property_exists($this, $field)) {
-			$attributes[$field] = $this->$field;
-		}
-	  }
-	  return $attributes;
-	}
-	
-	protected function sanitized_attributes() {
-	  global $mydb;
-	  $clean_attributes = array();
-	
-	  foreach($this->attributes() as $key => $value){
-	    $clean_attributes[$key] = $mydb->escape_value($value);
-	  }
-	  return $clean_attributes;
-	}
-	
-	
-	
-	public function save() {
-	
-	  return isset($this->id) ? $this->update() : $this->create();
-	}
-	
-	public function create() {
-		global $mydb;
-		
-		$attributes = $this->sanitized_attributes();
-		$sql = "INSERT INTO ".self::$tblname." (";
-		$sql .= join(", ", array_keys($attributes));
-		$sql .= ") VALUES ('";
-		$sql .= join("', '", array_values($attributes));
-		$sql .= "')";
-	echo $mydb->setQuery($sql);
-	
-	 if($mydb->executeQuery()) {
-	    $this->id = $mydb->insert_id();
-	    return true;
-	  } else {
-	    return false;
-	  }
-	}
+        if ($stmt === false) {
+            die("Error preparing statement: " . $mydb->error);
+        }
 
-	public function update($id='') {
-	  global $mydb;
-		$attributes = $this->sanitized_attributes();
-		$attribute_pairs = array();
-		foreach($attributes as $key => $value) {
-		  $attribute_pairs[] = "{$key}='{$value}'";
-		}
-		$sql = "UPDATE ".self::$tblname." SET ";
-		$sql .= join(", ", $attribute_pairs);
-		$sql .= " WHERE APPLICANTID='". $id."'";
-	  $mydb->setQuery($sql);
-	 	if(!$mydb->executeQuery()) return false; 	
-		
-	}
+        $stmt->bind_param("ss", $U_USERNAME, $h_pass);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-   public function APLupdate($id=0) {
-	  global $mydb;
-		$attributes = $this->sanitized_attributes();
-		$attribute_pairs = array();
-		foreach($attributes as $key => $value) {
-		  $attribute_pairs[] = "{$key}='{$value}'";
-		}
-		$sql = "UPDATE ".self::$tblname." SET ";
-		$sql .= join(", ", $attribute_pairs);
-		$sql .= " WHERE APPLICANTID=". $id;
-	  $mydb->setQuery($sql);
-	 	if(!$mydb->executeQuery()) return false; 	
-		
-	}
+        if ($result !== false) {
+            $row_count = $result->num_rows;
+            if ($row_count == 1) {
+                $emp_found = $result->fetch_object();
+                $_SESSION['APPLICANTID'] = $emp_found->APPLICANTID;
+                $_SESSION['USERNAME'] = $emp_found->USERNAME;
+                $result->close(); // Close the result set
+                return true;
+            } else {
+                $result->close(); // Close the result set
+                return false;
+            }
+        } else {
+            die("Error executing query: " . $mydb->error);
+        }
+    }
 
-	public function delete($id='') {
-		global $mydb;
-		  $sql = "DELETE FROM ".self::$tblname;
-		  $sql .= " WHERE APPLICANTID='". $id."'";
-		  $sql .= " LIMIT 1 ";
-		  $mydb->setQuery($sql);
-		  
-			if(!$mydb->executeQuery()) return false; 	
-	
-	}	
+    public static function instantiate($record) {
+        $object = new self;
 
+        foreach ($record as $attribute => $value) {
+            if ($object->has_attribute($attribute)) {
+                $object->$attribute = $value;
+            }
+        }
+        return $object;
+    }
 
+    private function has_attribute($attribute) {
+        return array_key_exists($attribute, $this->attributes());
+    }
+
+    protected function attributes() {
+        global $mydb;
+        $attributes = array();
+        foreach ($this->dbfields() as $field) {
+            if (property_exists($this, $field)) {
+                $attributes[$field] = $this->$field;
+            }
+        }
+        return $attributes;
+    }
+
+    protected function sanitized_attributes() {
+        global $mydb;
+        $clean_attributes = array();
+
+        foreach ($this->attributes() as $key => $value) {
+            $clean_attributes[$key] = $mydb->escape_value($value);
+        }
+        return $clean_attributes;
+    }
+
+    public function save() {
+        return isset($this->id) ? $this->update() : $this->create();
+    }
+
+    public function create() {
+        global $mydb;
+
+        $attributes = $this->sanitized_attributes();
+        $sql = "INSERT INTO " . self::$tblname . " (";
+        $sql .= join(", ", array_keys($attributes));
+        $sql .= ") VALUES ('";
+        $sql .= join("', '", array_values($attributes));
+        $sql .= "')";
+
+        echo $mydb->setQuery($sql);
+
+        if ($mydb->executeQuery()) {
+            $this->id = $mydb->insert_id();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function update($id = '') {
+        global $mydb;
+        $attributes = $this->sanitized_attributes();
+        $attribute_pairs = array();
+        foreach ($attributes as $key => $value) {
+            $attribute_pairs[] = "{$key}='{$value}'";
+        }
+        $sql = "UPDATE " . self::$tblname . " SET ";
+        $sql .= join(", ", $attribute_pairs);
+        $sql .= " WHERE APPLICANTID='" . $id . "'";
+        $mydb->setQuery($sql);
+        if (!$mydb->executeQuery()) return false;
+    }
+
+    public function APLupdate($id = 0) {
+        global $mydb;
+        $attributes = $this->sanitized_attributes();
+        $attribute_pairs = array();
+        foreach ($attributes as $key => $value) {
+            $attribute_pairs[] = "{$key}='{$value}'";
+        }
+        $sql = "UPDATE " . self::$tblname . " SET ";
+        $sql .= join(", ", $attribute_pairs);
+        $sql .= " WHERE APPLICANTID=" . $id;
+        $mydb->setQuery($sql);
+        if (!$mydb->executeQuery()) return false;
+    }
+
+    public function delete($id = '') {
+        global $mydb;
+        $sql = "DELETE FROM " . self::$tblname;
+        $sql .= " WHERE APPLICANTID='" . $id . "'";
+        $sql .= " LIMIT 1 ";
+        $mydb->setQuery($sql);
+
+        if (!$mydb->executeQuery()) return false;
+    }
 }
 ?>
