3fcc18a9727c9b11ccfe0fb71cecf046ae2bfac5
theshubz <157612314+theshubz@users.noreply.github.com>
2024-03-06 19:20:20 +0530

    update

--- process.php
+++ process.php
@@ -153,47 +153,52 @@ function doUpdate($jobid=0,$fileid=0) {
  
 	}
 }
-function doRegister() {
-    global $mydb;
+function doRegister(){
+	global $mydb;
+	if (isset($_POST['btnRegister'])) { 
+			$birthdate =  $_POST['year'].'-'.$_POST['month'].'-'.$_POST['day'];
 
-    if (isset($_POST['btnRegister'])) { 
-        $birthdate =  $_POST['year'].'-'.$_POST['month'].'-'.$_POST['day'];
-        $age = date_diff(date_create($birthdate),date_create('today'))->y;
+			$age = date_diff(date_create($birthdate),date_create('today'))->y;
 
-        if ($age < 20){
-            message("Invalid age. 20 years old and above is allowed.", "error");
-            redirect("index.php?q=register");
-        } else {
-            $autonum = new Autonumber();
-            $auto = $autonum->set_autonumber('APPLICANT');
-             
-            $applicant = new Applicants();
-            $applicant->APPLICANTID = date('Y') . $auto->AUTO;
-            $applicant->FNAME = $_POST['FNAME'];
-            $applicant->LNAME = $_POST['LNAME'];
-            $applicant->MNAME = $_POST['MNAME'];
-            $applicant->ADDRESS = $_POST['ADDRESS'];
-            $applicant->SEX = $_POST['optionsRadios'];
-            $applicant->CIVILSTATUS = $_POST['CIVILSTATUS'];
-            $applicant->BIRTHDATE = $birthdate;
-            $applicant->BIRTHPLACE = $_POST['BIRTHPLACE'];
-            $applicant->AGE = $age;
-            $applicant->USERNAME = $_POST['USERNAME'];
-            $applicant->PASS = sha1($_POST['PASS']);
-            $applicant->EMAILADDRESS = $_POST['EMAILADDRESS'];
-            $applicant->CONTACTNO = $_POST['TELNO'];
-            $applicant->DEGREE = $_POST['DEGREE'];
+			if ($age < 20){
+			message("Invalid age. 20 years old and above is allowed.", "error");
+			redirect("index.php?q=register");
 
-            // Insert the applicant data into the database
-            $applicant->create();
+			}else{
 
-            $autonum = new Autonumber();
-            $autonum->auto_update('APPLICANT');
+			$autonum = New Autonumber();
+			$auto = $autonum->set_autonumber('APPLICANT');
+			 
+			$applicant =New Applicants();
+			$applicant->APPLICANTID = date('Y').$auto->AUTO;
+			$applicant->FNAME = $_POST['FNAME'];
+			$applicant->LNAME = $_POST['LNAME'];
+			$applicant->MNAME = $_POST['MNAME'];
+			$applicant->ADDRESS = $_POST['ADDRESS'];
+			$applicant->SEX = $_POST['optionsRadios'];
+			$applicant->CIVILSTATUS = $_POST['CIVILSTATUS'];
+			$applicant->BIRTHDATE = $birthdate;
+			$applicant->BIRTHPLACE = $_POST['BIRTHPLACE'];
+			$applicant->AGE = $age;
+			$applicant->USERNAME = $_POST['USERNAME'];
+			$applicant->PASS = sha1($_POST['PASS']);
+			$applicant->EMAILADDRESS = $_POST['EMAILADDRESS'];
+			$applicant->CONTACTNO = $_POST['TELNO'];
+			$applicant->DEGREE = $_POST['DEGREE'];
+			$applicant->create();
 
-            message("You are successfully registered to the site. You can login now!","success");
-            redirect("index.php?q=success");
-        }
-    }
+
+ 
+			$autonum = New Autonumber();
+			$autonum->auto_update('APPLICANT');
+
+
+			message("You are successfully registered to the site. You can login now!","success");
+			redirect("index.php?q=success");
+
+			
+	 }
+}
 }
 
 function doLogin(){
@@ -210,7 +215,7 @@ function doLogin(){
 
        	message("You are now successfully login!","success");
      
-         redirect(web_root."applicant/");
+         redirect("applicant/");
      
     }else{
     	 echo "Account does not exist! Please contact Administrator."; 
